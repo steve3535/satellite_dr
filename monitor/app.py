c437cc144c6445614fae01ad8f7d2a3ba99c9c0f
mk417 <kwakou.missoh@lalux.lu>
2023-11-21 11:18:17 +0100

    loop+signal_handler

--- monitor/app.py
+++ monitor/app.py
@@ -1,16 +1,39 @@
 from flask import Flask, request
 import requests  
-from flask_cors import CORS 
+import time
+import sys
+from colorama import Fore,Style
+import signal 
 
 app = Flask(__name__)
-CORS(app)
-#@app.route("/monitor")
-def monitor():
-    url="http://127.0.0.1:9001/status/"
+
+
+def signal_handler(signal,frame):
+    print(Style.RESET_ALL+"Interrupted by user ...")
+    sys.exit(2)
+
+def check_status():
+    url="http://127.0.0.1:9001/status"
     res=requests.get(url)
     
-    res.json()
+    return res.json()
+
+def monitor():
+    print(Style.RESET_ALL)
+    while True:
+        x,y = check_status()[0]['live'],check_status()[1]['live']
+        if not(x or y):
+            print(Fore.RED+'!! CRIT !!'+Style.RESET_ALL+' >> both sites are down')
+        elif x and y:
+            print(Fore.RED+'!! CRIT !!'+Style.RESET_ALL+' >> both sites are up')
+        else:
+            print(Fore.GREEN+'STATUS OK')
+        time.sleep(5)
+
+
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0",debug=True,port=9002)
+    signal.signal(signal.SIGINT,signal_handler)
     monitor()
+    
