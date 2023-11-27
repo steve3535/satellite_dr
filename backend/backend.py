da0e384e7be85f61ce6be306aaa099f9724803fb
mk417 <kwakou.missoh@lalux.lu>
2023-11-27 14:31:34 +0100

    fix bugs

--- backend/backend.py
+++ backend/backend.py
@@ -4,20 +4,28 @@ from pyVmomi import vim
 import getpass
 from flask import Flask, request, jsonify
 import sys
-
+import os 
 
 MAX_DEPTH = 10
-servers = [{'name':"VSL-PRO-SAT-001_LU712_BKP1.1_D_PRO",'state':""},{'name':"VSL-PRO-SAT-002_LU712_DC3",'state':""}]
 
 app = Flask(__name__)
 
+host=os.environ['host']
+user=os.environ['user']
+pwd=os.environ['pwd']
+primary=os.environ['primary']
+secondary=os.environ['secondary']
+
+servers = [{'name':primary,'state':""},{'name':secondary,'state':""}]
+
 def connect():
     try:
-      context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-      context.verify_mode = ssl.CERT_NONE
-      si = SmartConnect(host="lu309.lalux.local", user="mk417@lalux.local", pwd="xxxxxxxxxx$$")
+      context = ssl._create_unverified_context() #ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+      context.verify_mode = ssl.CERT_NONE # disable ssl verification 
+      si = SmartConnect(host=host, user=user, pwd=pwd,sslContext=context)
     except Exception as e:
         print("an err occured while connecting to ESX")
+        print(e) 
         sys.exit(1)
     content = si.RetrieveContent()
     return content
@@ -62,14 +70,14 @@ def check(vm, depth=1):
         for child in vmlist:
             check(child, depth+1)
         return
-    if vm.summary.config.name == "VSL-PRO-SAT-002_LU712_DC3":
+    if vm.summary.config.name == secondary:
         servers[1]['state']=vm.summary.runtime.powerState
         if servers[1]['state'] == "poweredOn":
             servers[1]['live']=True
         else:
             servers[1]['live']=False 
                           
-    if vm.summary.config.name == "VSL-PRO-SAT-001_LU712_BKP1.1_D_PRO":
+    if vm.summary.config.name == primary:
         servers[0]['state']=vm.summary.runtime.powerState
         if servers[0]['state'] == "poweredOn":
             servers[0]['live']=True
@@ -84,7 +92,7 @@ def swap(vm, depth=1):
         for child in vmlist:
             swap(child, depth+1)
         return
-    if vm.summary.config.name in ("VSL-PRO-SAT-002_LU712_DC3","VSL-PRO-SAT-001_LU712_BKP1.1_D_PRO"):
+    if vm.summary.config.name in (primary,secondary):
        if vm.summary.runtime.powerState == "poweredOn":
            print(f"Shuting down {vm.summary.config.name} ...")
            vm.PowerOff()
@@ -132,4 +140,5 @@ def switch():
 # Start program
 if __name__ == "__main__":    
     app.run(host="0.0.0.0",debug=True,port=9001)   
+
     
